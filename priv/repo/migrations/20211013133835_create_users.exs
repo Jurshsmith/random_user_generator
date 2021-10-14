a079e60206be6527618e75e754ecc82555ab9c6a
Joshua Oladele <jurshsmith@gmail.com>
2021-10-14 17:12:07 +0100

    adds timestamps and defaults for user points

--- priv/repo/migrations/20211013133835_create_users.exs
+++ priv/repo/migrations/20211013133835_create_users.exs
@@ -3,9 +3,14 @@ defmodule RandomUserGenerator.Repo.Migrations.CreateUsers do
 
   def change do
     create table(:users) do
-      add :points, :integer
+      add :points, :integer, default: 0
 
       timestamps()
+    end
+
+    alter table(:users) do
+      modify(:inserted_at, :timestamp, default: fragment("NOW()"))
+      modify(:updated_at, :timestamp, default: fragment("NOW()"))
     end
   end
 end
