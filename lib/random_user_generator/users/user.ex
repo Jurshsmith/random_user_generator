d5a4e74edf57ac937444081fdfe9bb80ede0b5e6
Joshua Oladele <jurshsmith@gmail.com>
2021-10-14 17:52:01 +0100

    users context stuff

---
+++ lib/random_user_generator/users/user.ex
@@ -0,0 +1,22 @@
+defmodule RandomUserGenerator.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @type t :: %__MODULE__{
+          id: non_neg_integer,
+          points: integer()
+        }
+
+  schema "users" do
+    field :points, :integer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:points])
+    |> validate_required([:points])
+  end
+end
