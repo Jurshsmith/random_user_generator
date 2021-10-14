7b25e39d77e13caeaf41f0c26b639a998c6b779b
Joshua Oladele <jurshsmith@gmail.com>
2021-10-14 18:48:46 +0100

    implements naive solution to update the user points with a stream like strategy

--- lib/random_user_generator/generators/random_user.ex
+++ lib/random_user_generator/generators/random_user.ex
@@ -1,6 +1,13 @@
 defmodule RandomUserGenerator.RandomUser do
   alias RandomUserGenerator.Utils
 
+  # TODO: move to another module
+  import Ecto.Query
+  alias RandomUserGenerator.Repo
+  alias RandomUserGenerator.Users.User
+
+  @randomization_interval 60_000
+
   use GenServer
 
   def start_link(opts) do
@@ -9,6 +16,8 @@ defmodule RandomUserGenerator.RandomUser do
 
   @impl true
   def init(_opts) do
+    schedule_randomization()
+
     {:ok,
      %{
        max_number: generate_random_user_point(),
@@ -16,6 +25,72 @@ defmodule RandomUserGenerator.RandomUser do
      }}
   end
 
+  @impl true
+  def handle_info(:randomize, state) do
+    randomize_points_for_all_users()
+
+    schedule_randomization()
+
+    {:noreply, state |> Map.put(:max_number, generate_random_user_point())}
+  end
+
+  @impl true
+  def handle_info({task_ref, {:ok, true}}, state) do
+    IO.puts("Completed randomizing all users points")
+    Process.demonitor(task_ref, [:flush])
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({:DOWN, _ref, _, _, reason}, state) do
+    IO.puts("Task failed with reason #{inspect(reason)}")
+
+    {:noreply, state}
+  end
+
+  def randomize_points_for_all_users do
+    Task.Supervisor.async_nolink(__MODULE__.TaskSupervisor, fn ->
+      1..1_000_000
+      |> Stream.chunk_every(1_000)
+      |> Task.async_stream(
+        fn chunk ->
+          min = chunk |> Enum.at(0)
+          max = chunk |> Enum.at(-1)
+
+          from(user in User,
+            where: user.id >= ^min and user.id <= ^max,
+            update: [set: [points: fragment("floor(random() * (100 + 1))")]]
+          )
+          |> Repo.update_all([])
+        end,
+        max_concurrency: 10
+      )
+      |> Enum.reduce(fn _, _acc -> {:ok, true} end)
+    end)
+  end
+
+  def randomize_points_for_all_users_more_optimized do
+    Task.Supervisor.async_nolink(__MODULE__.TaskSupervisor, fn ->
+      1..4_000
+      |> Stream.chunk_every(1000)
+      |> Task.async_stream(fn chunk ->
+        min = chunk |> Enum.at(0)
+        max = chunk |> Enum.at(-1)
+
+        from(user in User,
+          where: user.id >= ^min and user.id <= ^max,
+          update: [set: [points: fragment("floor(random()*100)")]]
+        )
+        |> Repo.update_all([])
+      end)
+      |> Enum.reduce(fn _, _acc -> {:ok, true} end)
+    end)
+  end
+
   defp generate_random_user_point(), do: Utils.generate_random_number(100)
   defp get_timestamp(), do: Utils.get_timestamp()
+
+  defp schedule_randomization(interval \\ @randomization_interval),
+    do: Process.send_after(self(), :randomize, interval)
 end
